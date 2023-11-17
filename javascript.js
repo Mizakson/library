8503056702d8e546bb950ecc1f9e636ab9ec0912
Mizakson <maxwellizakson@gmail.com>
2023-11-17 11:44:36 -0500

    Modify addBookToLibrary function

--- javascript.js
+++ javascript.js
@@ -11,7 +11,16 @@ function Book() {
     // };
 };
 
+const defaultBook = {
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    pages: 295,
+    read: true
+}; 
 
-function addBookToLibrary() {
-    
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
+
+addBookToLibrary(defaultBook);
