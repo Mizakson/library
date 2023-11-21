c71e8a161ec43b31d5be7bc84825bb6d5bcfcde2
Mizakson <maxwellizakson@gmail.com>
2023-11-21 11:16:46 -0500

    Create displayLibrary function, add-book-form

--- javascript.js
+++ javascript.js
@@ -1,26 +1,44 @@
 const myLibrary = [];
 
-
+/* constructor */
 function Book() {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.read = read;
     // this.info = function() {
-    //     return `${title} by ${author}, ${pages}, ${read}`;
+    //     return `${title} by ${author}, ${read}`;
     // };
 };
 
-const defaultBook = {
+/* define 2 default books */
+const defaultBook1 = {
     title: 'The Hobbit',
     author: 'J.R.R. Tolkien',
-    pages: 295,
     read: true
 }; 
+
+const defaultBook2 = {
+    title: 'The War of the Worlds',
+    author: 'H.G. Wells',
+    read: false
+}
 
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-addBookToLibrary(defaultBook);
+/* add default books */
+addBookToLibrary(defaultBook1);
+addBookToLibrary(defaultBook2);
+
+/* iterate through array to display individual books */
+function displayLibrary(arr) {
+    let i = 0;
+    while (i < arr.length) {
+        console.log(arr[i]);
+        i++;
+    }
+}
+
+displayLibrary(myLibrary);
