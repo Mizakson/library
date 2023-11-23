0926b46a4d8e1ae9b53ae9cc1f0ee01d17fb26bc
Mizakson <maxwellizakson@gmail.com>
2023-11-23 18:20:17 -0500

    Modify book object prototypal inheritance

--- javascript.js
+++ javascript.js
@@ -8,13 +8,22 @@ function Book() {
     this.title = title;
     this.author = author;
     this.read = read;
+    this.displayInfo = function() {
+        return `${this.title}, ${this.author}, ${this.read}`;
+    };
 }
 
-const $title = document.getElementById('title');
-const $author = document.getElementById('author');
-const $read = document.getElementById('read');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const read = document.querySelector('#read');
+const bookshelf = document.querySelector('.bookshelf');
+const form = document.querySelector('form');
 
-function addBookToLibrary(book) {
-    book = new Book($title.value, $author.value, $read.value);
-    myLibrary.push(book);
+const book = new Book(title.value, author.value, read.value);
+Object.setPrototypeOf(book, Book);
+
+function addBookToLibrary() {
+    
 }
+
+form.addEventListener('submit', addBookToLibrary());
