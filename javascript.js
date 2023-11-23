1488dc356b6761091c95492504220ecc2a4ee0b8
Mizakson <maxwellizakson@gmail.com>
2023-11-23 17:35:11 -0500

    Create initial addBookToLibrary function

--- javascript.js
+++ javascript.js
@@ -7,5 +7,14 @@ const myLibrary = [];
 function Book() {
     this.title = title;
     this.author = author;
-    this.read = false;
+    this.read = read;
+}
+
+const $title = document.getElementById('title');
+const $author = document.getElementById('author');
+const $read = document.getElementById('read');
+
+function addBookToLibrary(book) {
+    book = new Book($title.value, $author.value, $read.value);
+    myLibrary.push(book);
 }
