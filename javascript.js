64a73fe3ed95b17b99a38a0dfa8036543fbaa20b
Mizakson <maxwellizakson@gmail.com>
2023-11-27 11:57:58 -0500

    Clean js file, add table column names

--- javascript.js
+++ javascript.js
@@ -16,9 +16,16 @@ function Book(title, author, read) {
 const defBook1 = new Book('The War of the Worlds', 'H.G. Wells', 'no');
 const defBook2 = new Book('Lord of the Flies', 'William Golding', 'yes');
 
-myLibrary.push(defBook1);
-myLibrary.push(defBook2);
+myLibrary.push(defBook1.displayInfo());
+myLibrary.push(defBook2.displayInfo());
 
 const form = document.getElementById('form');
 const bookshelf = document.querySelector('.bookshelf');
+const $title = document.getElementById('title');
+const $author = document.getElementById('author');
+const $read = document.getElementById('read');
+const table = document.getElementById('books');
+
+console.log(defBook1.displayInfo());
+console.log(defBook2.displayInfo());
 
