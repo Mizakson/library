7fab7b58571a8d5592de0d5906a253bd26019920
Mizakson <maxwellizakson@gmail.com>
2023-12-05 11:05:46 -0500

    Remove previous updateDisplay function

--- javascript.js
+++ javascript.js
@@ -131,35 +131,6 @@ function addBookToLibrary() {
 };
 
 
-
-// function updateDisplay() {
-//     newVal = newBook;
-//     const row = document.createElement('tr');
-//     row.id = 'book-data';
-//     for (item in newBook) {
-//         const cell = document.createElement('td');
-//         newBook.title = $title.value;
-//         newBook.author = $author.value;
-//         newBook.read = $read.value;
-//         cell.innerHTML = `${newVal[item]}`;
-//         row.appendChild(cell);
-//     }
-//     // for (let i = 0; i < 1; i++) {
-//     //     const btn = document.createElement('button');
-//     //     btn.innerText = 'X';
-//     //     btn.id = 'delete-btn';
-//     //     row.appendChild(btn);
-//     // }
-//     // for (let i = 0; i < 1; i++) {
-//     //     const btn = document.createElement('button');
-//     //     btn.innerText = 'Read Status';
-//     //     btn.id = 'read-status-btn';
-//     //     row.appendChild(btn);
-//     // }
-//     table.appendChild(row);
-// };
-
-
 function clearForm() {
     $title.value = '';
     $author.value = '';
