0c09ae6d772cb4dc3fdd0db8d26de27ed68ee1d0
Mizakson <maxwellizakson@gmail.com>
2023-12-05 17:52:39 -0500

    Refactor form submission logic

--- javascript.js
+++ javascript.js
@@ -20,16 +20,40 @@ myLibrary.push(defBook2);
 Object.setPrototypeOf(defBook1, Book);
 Object.setPrototypeOf(defBook2, Book);
 
-console.log(myLibrary);
+const newBookBtn = document.getElementById('add-book');
+const form = document.querySelector('form');
+const $title = document.getElementById('title');
+const $author = document.getElementById('author');
+const $pages = document.getElementById('pages');
+const $read = document.getElementById('read');
+
 
 function addBookToLibrary() {
-    const newBook = new Book();
+    const title = $title.value;
+    const author = $author.value;
+    const pages = $pages.value
+    const read = $read.value;
+    
+    const newBook = new Book(title, author, pages, read);
     Object.setPrototypeOf(newBook, Book);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+
 };
 
-const newEntryBtn = document.getElementById('add-book');
-const form = document.getElementById('add-book-form');
+function clearForm() {
+    $title.value = '';
+    $author.value = '';
+    $pages.value = '';
+}
 
-newEntryBtn.addEventListener('onclick', () => {
-    console.log('clicked');
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    clearForm();
+})
+
+newBookBtn.addEventListener("click", function() {
+    form.style.display = 'block';
+    console.log('NEW BOOK FORM');
 })
